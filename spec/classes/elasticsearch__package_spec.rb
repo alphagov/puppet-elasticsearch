971b82c8fae55c7916aeca4f573d860ff7e54be1
Garima Singh <igarimasingh@gmail.com>
2013-07-22 14:28:41 +0100

    [#51888499] Added elasticsearch::package tests

---
+++ spec/classes/elasticsearch__package_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'elasticsearch::package', :type => :class do
+  it "should fail when elasticsearch package version not specified" do
+    expect {
+      should contain_package('elasticsearch')
+    }.to raise_error(Puppet::Error, /You must provide an elasticsearch version for package/)
+  end
+
+  context "with specified version" do
+    let(:params) do
+      { :version => '1.2.3' }
+    end
+
+    it do
+      should contain_package('elasticsearch').with_ensure('1.2.3')
+    end
+  end
+end
