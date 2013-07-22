09c2ecec4966c7937e3d5d6debf30ed78f46bb6f
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-07-22 16:07:10 +0100

    remove old unused install.pp

--- manifests/install.pp
+++
@@ -1,9 +0,0 @@
-# == Class elasticsearch::intall
-#
-class elasticsearch::install {
-  include elasticsearch::params
-
-  package { $elasticsearch::params::package_name:
-    ensure => present,
-  }
-}
