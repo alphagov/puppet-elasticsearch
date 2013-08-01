f96b86d67d0b0cfdc3cd70e5a0085a1d8e89dd5f
Garima Singh <igarimasingh@gmail.com>
2013-08-01 14:00:20 +0100

    Fixing 80 char limit issues in pp files (to make lint pass)

--- manifests/install.pp
+++ manifests/install.pp
@@ -28,7 +28,9 @@ class elasticsearch::install(
   # job to manage elasticsearch in elasticsearch::{config,service}
   exec { 'disable-default-elasticsearch':
     onlyif      => '/usr/bin/test -f /etc/init.d/elasticsearch',
-    command     => '/etc/init.d/elasticsearch stop && /bin/rm -f /etc/init.d/elasticsearch && /usr/sbin/update-rc.d elasticsearch remove',
+    command     => "/etc/init.d/elasticsearch stop && \
+          /bin/rm -f /etc/init.d/elasticsearch && \
+          /usr/sbin/update-rc.d elasticsearch remove",
     refreshonly => true,
   }
 
